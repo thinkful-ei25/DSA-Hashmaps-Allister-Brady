2692f5801299f9018b638d00af3c8aafe4934180
Box <40639989+theBoxed@users.noreply.github.com>
2018-12-17 12:22:28 -0500

    working on palindrom'

--- hashmap.js
+++ hashmap.js
@@ -14,7 +14,7 @@ class HashMap {
 
   get(key) {
     const index = this._findSlot(key);
-    if(this._slots[index] === undefined) {
+    if (this._slots[index] === undefined) {
       throw new Error('Key Error');
     }
     return this._slots[index].value;
@@ -34,6 +34,7 @@ class HashMap {
   }
 
   _findSlot(key) {
+    // console.log('FINDSLOT => ' + key);
     const hash = HashMap._hashString(key);
     const start = hash % this._capacity;
 
@@ -53,13 +54,14 @@ class HashMap {
     this._slots = [];
 
     for (const slot of oldSlots) {
-      if(slot !== undefined) {
+      if (slot !== undefined) {
         this.set(slot.key, slot.value);
       }
     }
   }
 
   static _hashString(string) {
+    // console.log('HASHSTRING => ' + string);
     let hash = 5381;
     for (let i = 0; i < string.length; i++) {
       hash = (hash << 5) + hash + string.charCodeAt(i);
@@ -72,16 +74,56 @@ class HashMap {
 HashMap.MAX_LOAD_RATIO = 0.9;
 HashMap.SIZE_RATIO = 3;
 
+function createHashMap() {
 let lor = new HashMap();
-lor.set('Hobbit', "Bilbo");
-lor.set('Hobbit', "Frodo");
-lor.set('Wizard', "Gandolf");
-lor.set('Human', "Aragon");
-lor.set('Maiar', "The Necromancer");
-lor.set('Maiar', "Sauron");
-lor.set('RingBearer', "Gollum");
-lor.set('LadyOfLight', "Galadriel");
-lor.set('HalfElven', "Arwen");
-lor.set('Ent', "Treebeard");
+lor.set('Hobbit', 'Bilbo');
+lor.set('Hobbit', 'Frodo');
+lor.set('Wizard', 'Gandolf');
+lor.set('Human', 'Aragon');
+lor.set('Maiar', 'The Necromancer');
+lor.set('Maiar', 'Sauron');
+lor.set('RingBearer', 'Gollum');
+lor.set('LadyOfLight', 'Galadriel');
+lor.set('HalfElven', 'Arwen');
+lor.set('Ent', 'Treebeard');
 console.log(lor.get('Maiar'));
 console.log(lor);
+console.log(lor._findSlot('Hobbi'));
+}
+/*
+1. Check length of the string input if it's 1 or 0 then return false.
+2. Create a char hashmap
+3. Create a loop to run over each char in the str and place the letter as the key in the char hashmap
+4. If the key already exists then set its value to +1, if not, then add it as a key and set value to 1
+5. After it's been mapped, if the length of the str input is odd 
+6. If it's length is odd then check to make sure only 1 key has an odd value.
+  - If that's true, then check to make sure that the rest of the keys have a even value.
+7. If not odd, then check to make sure that no keys have an odd value.
+8. Else return false.
+*/
+function palindrome(str) {
+  // Check length of the string input if it's 1 or 0 then return false.
+  const strLength = str.length;
+  if (strLength < 2) {
+    return false;
+  }
+  // Create a char hashmap
+  // Create a loop to run over each char in the str and place the letter as the key in the char hashmap
+  // If the key already exists then set its value to + 1, if not, then add it as a key and set value to 1
+  let chars = new HashMap();
+  for(let i = 0; i < str.length; i++) {
+    let char = str[i];
+    try {
+      let charValue = chars.get(char);
+      charValue++;
+      chars.set(char, charValue);
+    } catch (error) {
+      chars.set(char, 1);
+    }
+  }
+
+  console.log(chars);
+}
+
+
+palindrome('hello');
