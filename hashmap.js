3e4002ccbc49207478f4b8ad15022d2732be1c9b
Allister Rooke <allisterg@gmail.com>
2018-12-17 15:30:08 -0500

    anagram grouping

--- hashmap.js
+++ hashmap.js
@@ -75,20 +75,20 @@ HashMap.MAX_LOAD_RATIO = 0.9;
 HashMap.SIZE_RATIO = 3;
 
 function createHashMap() {
-let lor = new HashMap();
-lor.set('Hobbit', 'Bilbo');
-lor.set('Hobbit', 'Frodo');
-lor.set('Wizard', 'Gandolf');
-lor.set('Human', 'Aragon');
-lor.set('Maiar', 'The Necromancer');
-lor.set('Maiar', 'Sauron');
-lor.set('RingBearer', 'Gollum');
-lor.set('LadyOfLight', 'Galadriel');
-lor.set('HalfElven', 'Arwen');
-lor.set('Ent', 'Treebeard');
-console.log(lor.get('Maiar'));
-console.log(lor);
-console.log(lor._findSlot('Hobbi'));
+  let lor = new HashMap();
+  lor.set('Hobbit', 'Bilbo');
+  lor.set('Hobbit', 'Frodo');
+  lor.set('Wizard', 'Gandolf');
+  lor.set('Human', 'Aragon');
+  lor.set('Maiar', 'The Necromancer');
+  lor.set('Maiar', 'Sauron');
+  lor.set('RingBearer', 'Gollum');
+  lor.set('LadyOfLight', 'Galadriel');
+  lor.set('HalfElven', 'Arwen');
+  lor.set('Ent', 'Treebeard');
+  console.log(lor.get('Maiar'));
+  console.log(lor);
+  console.log(lor._findSlot('Hobbi'));
 }
 /*
 1. Check length of the string input if it's 1 or 0 then return false.
@@ -111,19 +111,58 @@ function palindrome(str) {
   // Create a loop to run over each char in the str and place the letter as the key in the char hashmap
   // If the key already exists then set its value to + 1, if not, then add it as a key and set value to 1
   let chars = new HashMap();
-  for(let i = 0; i < str.length; i++) {
+  for (let i = 0; i < str.length; i++) {
     let char = str[i];
     try {
       let charValue = chars.get(char);
-      charValue++;
-      chars.set(char, charValue);
+      chars.set(char, ++charValue);
     } catch (error) {
       chars.set(char, 1);
     }
   }
-
-  console.log(chars);
+  let odds = 0;
+  for (let j = 0; j < chars.length && odds <= 1; j++) {
+    let value = chars._slots[j] ? chars._slots[j].value : 0;
+    odds += value % 2;
+    }
+  return (odds <= 1);
 }
 
+function anagramGroup(array) {
+  let anagrams = new HashMap();
+  for(let word of array) {
+    
+    let sortedWord = word.toLowerCase().split('').sort().join('')
+    console.log(sortedWord)
+    try {
+      let arr = anagrams.get(sortedWord)
+      anagrams.set(sortedWord, [...arr, word])
+    } catch {
+      anagrams.set(sortedWord, [word])
+    }
+  }
+  let anagramsArray = []
+  for (let words of anagrams._slots) {
+    if (words) {
+      anagramsArray.push(words.value);
+    }
+  }
+  return anagramsArray;
+  
+}
 
-palindrome('hello');
+console.log(anagramGroup(['east', 'cars', 'acre', 'arcs', 'teas', 'eats', 'race']));
+
+// input: ['east', 'cars', 'acre', 'arcs', 'teas', 'eats', 'race']
+// output: [['east', 'teas', 'eats'], ['cars', 'arcs'], ['acre', 'race']]
+
+//1. create hashmap
+//2. loop through input array
+//3. sort each word alphabetically
+//4. if the sorted word is not in the hashmap, create a hashmap key with that word and add the unsorted word to the value array
+//5. if the sorted word is in the hashmap, add the unsorted word to the value array at that key
+//6. loop through the keys in the hashmap and add the values to the output array
+//7. return the output array
+
+
+console.log(palindrome('madam'));
